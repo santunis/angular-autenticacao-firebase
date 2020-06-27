4fe7a08f28d54ed1f42d03861b28565d5a48df65
Santunis Monteiro de Oliveira <santunis.oliveira@fornecedores.sicoob.com.br>
2020-06-26 22:27:25 -0300

    Passos iniciais finalizado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyAWT-OdiYMLzIS306esfzDnXEHX5_dNsYo",
-    authDomain: "login-fireauth-2b1d7.firebaseapp.com",
-    databaseURL: "https://login-fireauth-2b1d7.firebaseio.com",
-    projectId: "login-fireauth-2b1d7",
-    storageBucket: "login-fireauth-2b1d7.appspot.com",
-    messagingSenderId: "389895711898",
-    appId: "1:389895711898:web:0d056546ba3b2482674323",
-    measurementId: "G-YN5WKGN6XV"
+    "Dados do firebase aqui - apikey"
   }
 };
 
